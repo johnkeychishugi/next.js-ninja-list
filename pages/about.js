0551c46178f257fb271ba06168f91d3bc1b60dad
John Chishugi <jkchishugi@gmail.com>
2022-04-29 14:43:10 +0200

    ft-pages-routes

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About page</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inventore asperiores possimus ea officiis! Consequatur quod aliquid accusamus rerum veniam incidunt perspiciatis cupiditate magnam nisi aut!</p>
+        </div>
+     );
+}
+ 
+export default About;
