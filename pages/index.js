15f7375df487d5f338925dbc9c54615fbb3efe99
John Chishugi <jkchishugi@gmail.com>
2022-04-30 10:34:52 +0200

    ft-link-component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/footer'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Home page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inv </p>
+      <Link href="/ninjas">
+        <a>See Ninja Listing</a>
+      </Link>
+      <Footer />
     </div>
   )
 }
