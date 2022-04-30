9c9db5a542988559b54eb70fb73e4fef155f45b2
John Chishugi <jkchishugi@gmail.com>
2022-04-30 11:01:35 +0200

    ft-adding-style

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,15 @@
-import Navbar from '../components/Navbar'
-import Footer from '../components/footer'
+import styles from '../styles/Home.module.css';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <h1>Home page</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inv </p>
+      <h1 className={styles.title}>Home page</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inv </p>
       <Link href="/ninjas">
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   )
 }
