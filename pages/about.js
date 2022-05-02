93d04224beb35197c362a7c09b5db95d30d59d5f
John Chishugi <jkchishugi@gmail.com>
2022-05-02 23:11:28 +0200

    Ft-customazed the 404 page

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,18 @@
+import Head from "next/head";
+
 const About = () => {
     return ( 
+       <>
+         <Head>
+            <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas" />
+        </Head>
         <div>
             <h1>About page</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inventore asperiores possimus ea officiis! Consequatur quod aliquid accusamus rerum veniam incidunt perspiciatis cupiditate magnam nisi aut!</p>
         </div>
+       </>
      );
 }
  
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Home page</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inv </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Home page</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex aliquid temporibus dolores, voluptas quibusdam facilis? Aperiam corporis libero velit illum corrupti, quae ut amet debitis odio ipsa fugiat nam commodi.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit fuga aspernatur doloremque quos inv </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
+    
   )
 }
